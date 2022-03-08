8d5b66139106953c57e1a789e4b7841a071a9328
Osama M'hiri <mohamedoussema.mhiri@ensi-uma.tn>
2022-03-08 11:20:00 +0000

    del

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
 const log = require ('ololog')
+var colors = require('ansicolors');
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -9,5 +10,21 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, () => {
+ 
+// foreground colors
+var redHerring = colors.red('herring');
+var blueMoon = colors.blue('moon');
+ 
+console.log(redHerring);      // this will print 'herring' in red
+console.log(blueMoon);        // this 'moon' in blue
+ 
+// background colors
+console.log(colors.bgYellow('printed on yellow background'));
+  
+// mixing background and foreground colors
+// below two lines have same result (order in which bg and fg are combined doesn't matter)
+console.log(colors.bgYellow(colors.blue('printed on yellow background in blue')));
+console.log(colors.blue(colors.bgYellow('printed on yellow background in blue')));
+
   console.log(`Server running on http://localhost:${port}/`);
 });
